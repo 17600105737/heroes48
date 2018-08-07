603389f95db481691574a5a551a6b8a2fa20270a
chen <17600105737@163.com>
2018-08-07 14:40:35 +0800

    配置路由规则和导航菜单

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './assets/css/index.css';
+
 import router from './routes/router';
+
 new Vue({
   router,
   el: '#app',
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,22 @@
 import VueRouter from 'vue-router';
 import Vue from 'vue';
+import HeroList from '../views/hero/heroList';
+import WeaponList from '../views/weapon/weaponList';
+import EquipList from '../views/equip/equipList';
 
+// 挂载路由
 Vue.use(VueRouter);
 
+// 定义路由
 var router = new VueRouter({
-    routes: []
+    linkExactActiveClass: 'active',
+    routes: [
+        {path:'/',redirect:'/hero'},
+        {name:'hero',path:'/hero',component:HeroList},
+        {name:'weapon',path:'/weapon',component:WeaponList},
+        {name:'equip',path:'/equip',component:EquipList}
+    ]
 });
 
+// 导出路由
 export default router;
