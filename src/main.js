f392cd089e7d11eaed9101d5c8173188b98aa32f
chen <17600105737@163.com>
2018-08-07 11:52:05 +0800

    路由配置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
-
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './assets/css/index.css';
-
+import router from './routes/router';
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 })
