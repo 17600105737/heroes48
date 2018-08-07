9cc2b50d3de403860c1b7f22d5fb9836e651a1f4
chen <17600105737@163.com>
2018-08-07 11:15:29 +0800

    AppSiderbar

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import './assets/css/index.css'
+import './assets/css/index.css';
 
 new Vue({
   el: '#app',
